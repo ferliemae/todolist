b0e478f2c8fe0d445693523e58c00fe733e0e2dd
Ferlie Mae Penido <ferlspenido@gmail.com>
2022-01-04 13:08:18 +0800

    updated number of spaces

--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -2,10 +2,10 @@ import React from "react";
 import "./Todo.css";
 
 class Todo extends React.Component {
-    render() {
-        const { description, completed } = this.props.todo;
-        return <div className={completed ? 'completed' : ''}>{description}</div>
-    }
+  render() {
+    const { description, completed } = this.props.todo;
+    return <div className={completed ? 'completed' : ''}>{description}</div>
+  }
 }
 
 export default Todo;
