3f3e2ac3926038f4c31bbf5b2923eadd2890ac43
Ferlie Mae Penido <ferliemaepenido@Ferlies-MacBook-Pro.local>
2022-01-04 11:38:07 +0800

    organized routes and removed unused import

---
+++ server/routes.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router  = express.Router();
+
+router.get('/', function(req, res) {
+    res.json({ message: 'Welcome to our Todo List API!' });
+});
+router.use('/todos', require('./todo'));
+
+module.exports = router;
---
+++ server/todo.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router  = express.Router();
+var Todo    = require('./app/models/todo');
+
+router.route('/')
+    .post(function(req, res) {
+        const { description, completed } = req.body;
+        var todo = new Todo({description: description, completed: completed});
+
+        todo.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Todo created!' });
+        });
+    })
+    .get(function(req, res) {
+        Todo.find(function(err, todos) {
+            if (err) {
+                res.send(err);
+            }
+
+            res.json(todos);
+        })
+    })
+
+module.exports = router;
